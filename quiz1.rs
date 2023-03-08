057d7dfe0895da19a66d8772bfcccb2bed096f7b
Travis Horn <travis@travishorn.com>
2023-03-08 11:27:36 -0600

    Complete quiz1

--- quiz1.rs
+++ quiz1.rs
@@ -1,21 +1,21 @@
-// quiz1.rs
-// This is a quiz for the following sections:
-// - Variables
-// - Functions
-// - If
-
 // Mary is buying apples. The price of an apple is calculated as follows:
 // - An apple costs 2 rustbucks.
 // - If Mary buys more than 40 apples, each apple only costs 1 rustbuck!
-// Write a function that calculates the price of an order of apples given
-// the quantity bought. No hints this time!
+// Calculate the price of an order of apples given the quantity bought
 
-// I AM NOT DONE
+// Accept an argument called quantity. It is always a positive integer between 35 and 65 so u8 works
+// well. The return value will be a positive integer between 41 and 80 so u8 works here, too.
+fn calculate_price_of_apples(quantity: u8) -> u8 {
+    // Use an `if` to determine whether the quantity is more than 40
+    if quantity > 40 {
+        // If so, each apple costs 1 rustbuck, so the price is quantity * 1, or just simply quantity
+        quantity
+    } else {
+        // If not, each apple costs 2 rustbucks, so the price is quantity * 1
+        quantity * 2
+    }
+}
 
-// Put your function here!
-// fn calculate_price_of_apples {
-
-// Don't modify this function!
 #[test]
 fn verify_test() {
     let price1 = calculate_price_of_apples(35);
