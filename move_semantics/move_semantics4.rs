4d112788443a984214c388a66df6bad32838ed9b
Travis Horn <travis@travishorn.com>
2023-03-08 13:24:20 -0600

    Complete move_semantics4

--- move_semantics/move_semantics4.rs
+++ move_semantics/move_semantics4.rs
@@ -1,15 +1,9 @@
-// move_semantics4.rs
-// Refactor this code so that instead of passing `vec0` into the `fill_vec` function,
-// the Vector gets created in the function itself and passed back to the main
-// function.
-// Execute `rustlings hint move_semantics4` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
+// Instead of passing `vec0` into the `fill_vec` function, the Vector gets created in the function
+// itself and passed back to the main function.
 
 fn main() {
-    let vec0 = Vec::new();
-
-    let mut vec1 = fill_vec(vec0);
+    // `fill_vec` no longer takes any arguments
+    let mut vec1 = fill_vec();
 
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
@@ -20,7 +14,8 @@ fn main() {
 
 // `fill_vec()` no longer takes `vec: Vec<i32>` as argument
 fn fill_vec() -> Vec<i32> {
-    let mut vec = vec;
+    // vec is now created as a mutable new vector
+    let mut vec = Vec::new();
 
     vec.push(22);
     vec.push(44);
