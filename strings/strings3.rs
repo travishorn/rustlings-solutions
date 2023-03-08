fdaa323a13a9c5ea935229da5d1d7f9b1b4b0f8c
Travis Horn <travis@travishorn.com>
2023-03-08 15:37:20 -0600

    Complete strings3

--- strings/strings3.rs
+++ strings/strings3.rs
@@ -1,21 +1,28 @@
-// strings3.rs
-// Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
-    // TODO: Remove whitespace from both ends of a string!
-    ???
+    // Remove whitespace from both ends of a string!
+    // `.trim()` returns another `&str` since it is just a slice of the original data. That way, it
+    // is more efficient for memory management. So we use `to_string()` to coerce it into the
+    // expected `String` type
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
-    // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    // Add " world!" to the string!
+    // The `+` operator can concatenate strings, but not string literals (`&str`s). This is because
+    // It takes ownership of the left-hand side of the operation. Since `input` is a `&str`, we
+    // cannot use take ownership of it, and cannot use `+`. We can, however, use the `format!` macro
+    // instead.
+    format!("{}{}", input, " world!")
+
+    // Another option, if we were using `input` more than once, would be to create a single new
+    // `String instance with `let mut result = input.to_owned();` and then modify it as many times
+    // as necessary. For example, we could append to it with `result.push_str(" world!");`.
 }
 
 fn replace_me(input: &str) -> String {
-    // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    // Replace "cars" in the string with "balloons"!
+    // `.replace()` returns a `String` because it is mutating `input`
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
@@ -37,7 +44,13 @@ mod tests {
 
     #[test]
     fn replace_a_string() {
-        assert_eq!(replace_me("I think cars are cool"), "I think balloons are cool");
-        assert_eq!(replace_me("I love to look at cars"), "I love to look at balloons");
+        assert_eq!(
+            replace_me("I think cars are cool"),
+            "I think balloons are cool"
+        );
+        assert_eq!(
+            replace_me("I love to look at cars"),
+            "I love to look at balloons"
+        );
     }
 }
