408a8f350305c7627ed330fc23b81394ecd01aaa
Travis Horn <travis@travishorn.com>
2023-03-08 10:35:25 -0600

    Complete variables1

--- variables/variables1.rs
+++ variables/variables1.rs
@@ -1,10 +1,5 @@
-// variables1.rs
-// Make me compile!
-// Execute `rustlings hint variables1` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
-
 fn main() {
-    x = 5;
+    // `let` is used to declare a new variable and bind it to a value
+    let x = 5;
     println!("x has the value {}", x);
 }
