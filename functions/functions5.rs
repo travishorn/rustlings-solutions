01ad6499d4f5afdab71c321b34f68520d7f18514
Travis Horn <travis@travishorn.com>
2023-03-08 11:10:15 -0600

    Complete functions5

--- functions/functions5.rs
+++ functions/functions5.rs
@@ -1,13 +1,12 @@
-// functions5.rs
-// Execute `rustlings hint functions5` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
-
 fn main() {
     let answer = square(3);
     println!("The square of 3 is {}", answer);
 }
 
 fn square(num: i32) -> i32 {
-    num * num;
+    // Lines without a semicolon (;) at the end are considered expressions and return a value
+    num * num
+
+    // Alternatively, you can return a value with the `return` keyword
+    // return num * num;
 }
