27a852dc1b12cd34abc7c1b8e055256f1fdbe249
Travis Horn <travis@travishorn.com>
2023-03-08 14:13:04 -0600

    Complete structs2

--- structs/structs2.rs
+++ structs/structs2.rs
@@ -1,9 +1,3 @@
-// structs2.rs
-// Address all the TODOs to make the tests pass!
-// Execute `rustlings hint structs2` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
-
 #[derive(Debug)]
 struct Order {
     name: String,
@@ -34,8 +28,18 @@ mod tests {
     #[test]
     fn your_order() {
         let order_template = create_order_template();
-        // TODO: Create your own order using the update syntax and template above!
-        // let your_order =
+
+        // Create an order using the update syntax and template above!
+        let your_order = Order {
+            // Fields to update
+            name: String::from("Hacker in Rust"),
+            count: 1,
+
+            // Followed by the `..` syntax that specifies that the remaining fields  should have the
+            // same value as the order template
+            ..order_template
+        };
+
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
         assert_eq!(your_order.made_by_phone, order_template.made_by_phone);
