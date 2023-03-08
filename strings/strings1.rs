9bfe682408e7f915fd9e35629dd05eadd3711603
Travis Horn <travis@travishorn.com>
2023-03-08 14:54:54 -0600

    Complete strings1

--- strings/strings1.rs
+++ strings/strings1.rs
@@ -1,14 +1,13 @@
-// strings1.rs
-// Make me compile without changing the function signature!
-// Execute `rustlings hint strings1` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
-
 fn main() {
     let answer = current_favorite_color();
     println!("My current favorite color is {}", answer);
 }
 
 fn current_favorite_color() -> String {
-    "blue"
+    // This function's return type is `String`. `"blue"` looks like a `String`, but is actually a
+    // string literal of type `&str`. String literals are actually a slice of bytes `&[u8]` that are
+    // encoded in UTF-8. This means they are immutable and cannot be modified at runtime like
+    // `String`s can. Use the `to_string()` method on a string literal to copy its contents into a
+    // new `String`.
+    "blue".to_string()
 }
