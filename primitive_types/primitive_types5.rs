9d141a559b23c44e745c4cf593a7b54ed8dc8bf6
Travis Horn <travis@travishorn.com>
2023-03-08 11:48:39 -0600

    Completed primitive_types5

--- primitive_types/primitive_types5.rs
+++ primitive_types/primitive_types5.rs
@@ -1,12 +1,9 @@
-// primitive_types5.rs
-// Destructure the `cat` tuple so that the println will work.
-// Execute `rustlings hint primitive_types5` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
-
 fn main() {
     let cat = ("Furry McFurson", 3.5);
-    let /* your pattern here */ = cat;
+
+    // You can destructure tuples. The variables `name` and `age` will reference the first and
+    // second elements (respectively) in the cat tuple
+    let (name, age) = cat;
 
     println!("{} is {} years old.", name, age);
 }
