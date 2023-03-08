c4642277feb95f0644d622520d539fe42144dcee
Travis Horn <travis@travishorn.com>
2023-03-08 11:18:36 -0600

    Complete if2

--- if/if2.rs
+++ if/if2.rs
@@ -1,20 +1,14 @@
-// if2.rs
-
-// Step 1: Make me compile!
-// Step 2: Get the bar_for_fuzz and default_to_baz tests passing!
-// Execute `rustlings hint if2` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
-
 pub fn foo_if_fizz(fizzish: &str) -> &str {
     if fizzish == "fizz" {
         "foo"
+    // Other ifs can be chained after elses
+    } else if fizzish == "fuzz" {
+        "bar"
     } else {
-        1
+        "baz"
     }
 }
 
-// No test changes needed!
 #[cfg(test)]
 mod tests {
     use super::*;
