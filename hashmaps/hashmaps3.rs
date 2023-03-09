b2fa55b92fcec80e347f69c7902882cdc8148521
Travis Horn <travis@travishorn.com>
2023-03-09 09:58:05 -0600

    Complete hashmaps3

--- hashmaps/hashmaps3.rs
+++ hashmaps/hashmaps3.rs
@@ -1,20 +1,10 @@
-// hashmaps3.rs
-
 // A list of scores (one per line) of a soccer match is given. Each line
 // is of the form :
 // <team_1_name>,<team_2_name>,<team_1_goals>,<team_2_goals>
 // Example: England,France,4,2 (England scored 4 goals, France 2).
 
-// You have to build a scores table containing the name of the team, goals
-// the team scored, and goals the team conceded. One approach to build
-// the scores table is to use a Hashmap. The solution is partially
-// written to use a Hashmap, complete it to pass the test.
-
-// Make me pass the tests!
-
-// Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
+// Build a scores table containing the name of the team, goals
+// the team scored, and goals the team conceded using a HashMap.
 
 use std::collections::HashMap;
 
@@ -35,11 +25,37 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be the number of goals conceded from team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
+
+        // With the `scores` HashMap...
+        scores
+            // Use `entry()` to get a reference to an existing entry for the team
+            // We must use `clone()` on the team name because we use this name later
+            .entry(team_1_name.clone())
+            // `and_modify()` gets used if the entry already exists
+            .and_modify(|team| {
+                // Update the goals scored and the goals conceded
+                team.goals_scored += team_1_score;
+                team.goals_conceded += team_2_score;
+            })
+            // `or_insert()` gets used if the entry does not exist yet. Insert a new `Team` struct
+            .or_insert(Team {
+                name: team_1_name,
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score,
+            });
+
+        // Repeat for team 2 on the current line
+        scores
+            .entry(team_2_name.clone())
+            .and_modify(|team| {
+                team.goals_scored += team_2_score;
+                team.goals_conceded += team_1_score;
+            })
+            .or_insert(Team {
+                name: team_2_name,
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score,
+            });
     }
     scores
 }
