1b75f818a4161cc4238452ed56cee222e26f4398
Travis Horn <travis@travishorn.com>
2023-03-09 11:19:37 -0600

    Complete options3

--- options/options3.rs
+++ options/options3.rs
@@ -1,8 +1,3 @@
-// options3.rs
-// Execute `rustlings hint options3` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
-
 struct Point {
     x: i32,
     y: i32,
@@ -12,8 +7,11 @@ fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
     match y {
-        Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
+        // `y` gets partially moved during this pattern match. We can avoid this by using the `ref`
+        // keyword to borrow the value instead of moving it. We need to do that here since we use
+        // `y` later
+        Some(ref p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => println!("no match"),
     }
-    y; // Fix without deleting this line.
+    y;
 }
