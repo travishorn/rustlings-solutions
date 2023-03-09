fafd46acdc6f9685d696baefcb440b56ca84539d
Travis Horn <travis@travishorn.com>
2023-03-09 11:11:41 -0600

    Complete options2

--- options/options2.rs
+++ options/options2.rs
@@ -1,8 +1,3 @@
-// options2.rs
-// Execute `rustlings hint options2` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     #[test]
@@ -10,9 +5,12 @@ mod tests {
         let target = "rustlings";
         let optional_target = Some(target);
 
-        // TODO: Make this an if let statement whose value is "Some" type
-        word = optional_target {
-            assert_eq!(word, target);
+        // Initialize `word` equal to `optional_target`. Since `optional_target` is of type
+        // `Option<&str>`, `word` is also of type `Option<&str>`.
+        if let word = optional_target {
+            // This assertion is true because `Some(target)` wraps the string literal in `Option`.
+            // This matches `word` which is the same string slice also wrapped in `Option`.
+            assert_eq!(word, Some(target));
         }
     }
 
@@ -24,11 +22,19 @@ mod tests {
             optional_integers.push(Some(i));
         }
 
-        // TODO: make this a while let statement - remember that vector.pop also adds another layer of Option<T>
-        // You can stack `Option<T>`'s into while let and if let
-        integer = optional_integers.pop() {
-            assert_eq!(integer, range);
-            range -= 1;
+        // `optional_integers` is a vector of integers, each wrapped in `Option`. `pop()` wraps it's
+        // values in `Option`, as well (because it's possible that we try to pop an empty vector).
+        // This means that the value in this `while` loop will be double-wrapped in `Option`. We can
+        // use `Some()` to peel back one of those layers. What we're left with is an
+        // `Option`-wrapped integer, or `None`.
+        while let Some(integer) = optional_integers.pop() {
+            // If there is a value in the option-wrapped integer (not `None`)...
+            if let Some(value) = integer {
+                // Assert that the value is equal to the current range
+                assert_eq!(value, range);
+                // Decrement the range before continuing the `while` loop
+                range -= 1;
+            }
         }
     }
 }
