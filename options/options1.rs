f6ff27b2355e656eb83417b3db862c9effaac0b7
Travis Horn <travis@travishorn.com>
2023-03-09 10:47:07 -0600

    Complete options1

--- options/options1.rs
+++ options/options1.rs
@@ -1,16 +1,21 @@
-// options1.rs
-// Execute `rustlings hint options1` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
-
 // This function returns how much icecream there is left in the fridge.
 // If it's before 10PM, there's 5 pieces left. At 10PM, someone eats them
 // all, so there'll be no more left :(
 fn maybe_icecream(time_of_day: u16) -> Option<u16> {
     // We use the 24-hour system here, so 10PM is a value of 22 and 12AM is a value of 0
-    // The Option output should gracefully handle cases where time_of_day > 23.
-    // TODO: Complete the function body - remember to return an Option!
-    ???
+    // The Option output gracefully handles cases where time_of_day > 23.
+
+    // Match on the time of day
+    match time_of_day {
+        // Between 0 and 21 (12AM and 9PM), return 5. Wrap in `Some()` so it's of type `Option`
+        0..=21 => Some(5),
+
+        // Between 22 and 24 (10PM and 12AM), return 0.
+        22..=24 => Some(0),
+
+        // Any other time of day should not exist, return `None`
+        _ => None,
+    }
 }
 
 #[cfg(test)]
@@ -28,8 +33,11 @@ mod tests {
 
     #[test]
     fn raw_value() {
-        // TODO: Fix this test. How do you get at the value contained in the Option?
         let icecreams = maybe_icecream(12);
-        assert_eq!(icecreams, 5);
+
+        // `icereams` is of type `Option<u16>` because it's the return value from the
+        // `maybe_icecream` function. The right half of the assertion must be of type `Option<u16>`,
+        // as well. `Some(u16)` and `None` qualify.
+        assert_eq!(icecreams, Some(5));
     }
 }
