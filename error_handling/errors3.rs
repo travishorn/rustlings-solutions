5c1bfadb82b6245ee3a657bff2bbd83b6b3d35eb
Travis Horn <travis@travishorn.com>
2023-03-10 08:45:28 -0600

    Complete errors3

--- error_handling/errors3.rs
+++ error_handling/errors3.rs
@@ -1,14 +1,12 @@
-// errors3.rs
-// This is a program that is trying to use a completed version of the
-// `total_cost` function from the previous exercise. It's not working though!
-// Why not? What should we do to fix it?
-// Execute `rustlings hint errors3` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
+// This is a program that uses a completed version of the `total_cost` function from the previous
+// exercise.
 
 use std::num::ParseIntError;
 
-fn main() {
+// Since we use `?` to propagate errors inside this function, we need to define the return type as
+// `Result<(), ParseIntError>`. `()` is the unit type. Basically, we're saying we don't really care
+// about the return value if it's not an error.
+fn main() -> Result<(), ParseIntError> {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
@@ -20,6 +18,9 @@ fn main() {
         tokens -= cost;
         println!("You now have {} tokens.", tokens);
     }
+
+    // If we got to this point, the function has done its job. We can return `Ok` with a unit `()`
+    Ok(())
 }
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
