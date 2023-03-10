c04ad56af2f0c106555f3ce5a960b17582179e1c
Travis Horn <travis@travishorn.com>
2023-03-10 11:11:35 -0600

    Complete traits2

--- traits/traits2.rs
+++ traits/traits2.rs
@@ -1,23 +1,22 @@
-// traits2.rs
+// Implements the trait `AppendBar` for a vector of strings.
 //
-// Your task is to implement the trait
-// `AppendBar` for a vector of strings.
-//
-// To implement this trait, consider for
-// a moment what it means to 'append "Bar"'
-// to a vector of strings.
-//
-// No boiler plate code this time,
-// you can do this!
-// Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
+// Append "Bar" to a vector of strings means to push the value "Bar" into the vector
 
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
-// TODO: Implement trait `AppendBar` for a vector of strings.
+// Implement trait `AppendBar` for a vector of strings.
+impl AppendBar for Vec<String> {
+    // Implement `AppendBar` for type `Vec<String>`. `Vec<String>` implements the `Copy` trait which
+    // means it is copied by default instead of being moved. We can use `mut self` instead of `&mut
+    // self` since we have a copy of the data and not just a reference.
+    fn append_bar(mut self) -> Vec<String> {
+        // Push "Bar" into the vector and return it
+        self.push(String::from("Bar"));
+        self
+    }
+}
 
 #[cfg(test)]
 mod tests {
