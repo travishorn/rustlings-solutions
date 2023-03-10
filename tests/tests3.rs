6fb626c558eafb40670743eca68443c89480aec8
Travis Horn <travis@travishorn.com>
2023-03-10 11:50:48 -0600

    Complete tests3

--- tests/tests3.rs
+++ tests/tests3.rs
@@ -1,11 +1,3 @@
-// tests3.rs
-// This test isn't testing our function -- make it do that in such a way that
-// the test passes. Then write a second test that tests whether we get the result
-// we expect to get when we call `is_even(5)`.
-// Execute `rustlings hint tests3` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
-
 pub fn is_even(num: i32) -> bool {
     num % 2 == 0
 }
@@ -14,13 +6,18 @@ pub fn is_even(num: i32) -> bool {
 mod tests {
     use super::*;
 
+    // This test tests our function in such a way that the test passes.
     #[test]
     fn is_true_when_even() {
-        assert!();
+        assert!(is_even(0));
     }
 
+    // This second test tests whether we get the result we expect to get when we call `is_even(5)`.
     #[test]
     fn is_false_when_odd() {
-        assert!();
+        assert!(is_even(5) == false);
+
+        // This test could also be written with `assert_eq!()` like so
+        // assert_eq!(is_even(5), false);
     }
 }
