d5a79dbc3b24fb47e6e4aa9e7ce3bec337466367
Travis Horn <travis@travishorn.com>
2023-03-10 11:41:43 -0600

    Complete quiz3

--- quiz3.rs
+++ quiz3.rs
@@ -1,31 +1,28 @@
-// quiz3.rs
-// This quiz tests:
-// - Generics
-// - Traits
-// An imaginary magical school has a new report card generation system written in Rust!
-// Currently the system only supports creating report cards where the student's grade
-// is represented numerically (e.g. 1.0 -> 5.5).
-// However, the school also issues alphabetical grades (A+ -> F-) and needs
-// to be able to print both types of report card!
+// An imaginary magical school has a new report card generation system written in Rust! Currently
+// the system only supports creating report cards where the student's grade is represented
+// numerically (e.g. 1.0 -> 5.5). However, the school also issues alphabetical grades (A+ -> F-) and
+// needs to be able to print both types of report card!
 
-// Make the necessary code changes in the struct ReportCard and the impl block
-// to support alphabetical report cards. Change the Grade in the second test to "A+"
-// to show that your changes allow alphabetical grades.
+// The struct ReportCard and the impl block support alphabetical report cards. The Grade in the
+// second test is "A+" to show that it allows alphabetical grades.
 
-// Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
-
-pub struct ReportCard {
-    pub grade: f32,
+// This `ReportCard` struct supports a generic type `T`
+pub struct ReportCard<T> {
+    // `grade` uses the generic type `T` because it may be given an `f32` or a `String`
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+// To use the generic type `T`, both the `impl` and the name must be followed by `<T>`. We also need
+// to define how to display the generic type, otherwise `&self.grade` cannot be formatted using
+// `format!`. We do this with `<T: std::fmt::Display>`.
+impl<T: std::fmt::Display> ReportCard<T> {
     pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+        format!(
+            "{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, &self.grade
+        )
     }
 }
 
@@ -50,7 +47,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+".to_string(),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
