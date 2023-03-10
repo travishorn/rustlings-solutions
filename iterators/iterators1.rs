87c24f9c3b6857de3345663dcf84702545623fdf
Travis Horn <travis@travishorn.com>
2023-03-10 13:46:30 -0600

    Complete iterators1

--- iterators/iterators1.rs
+++ iterators/iterators1.rs
@@ -1,24 +1,21 @@
-// iterators1.rs
-//
-//  Make me compile by filling in the `???`s
-//
 // When performing operations on elements within a collection, iterators are essential.
 // This module helps you get familiar with the structure of using an iterator and
 // how to go through elements within an iterable collection.
-//
-// Execute `rustlings hint iterators1` or use the `hint` watch subcommand for a hint.
-
 // I AM NOT DONE
 
-fn main () {
+fn main() {
     let my_fav_fruits = vec!["banana", "custard apple", "avocado", "peach", "raspberry"];
 
-    let mut my_iterable_fav_fruits = ???;   // TODO: Step 1
+    // Get an iterator from `my_fav_fruits`
+    let mut my_iterable_fav_fruits = my_fav_fruits.iter();
 
+    // One-by-one, go through each element in the vector and assert its value
     assert_eq!(my_iterable_fav_fruits.next(), Some(&"banana"));
-    assert_eq!(my_iterable_fav_fruits.next(), ???);     // TODO: Step 2
+    assert_eq!(my_iterable_fav_fruits.next(), Some(&"custard apple"));
     assert_eq!(my_iterable_fav_fruits.next(), Some(&"avocado"));
-    assert_eq!(my_iterable_fav_fruits.next(), ???);     // TODO: Step 3
+    assert_eq!(my_iterable_fav_fruits.next(), Some(&"peach"));
     assert_eq!(my_iterable_fav_fruits.next(), Some(&"raspberry"));
-    assert_eq!(my_iterable_fav_fruits.next(), ???);     // TODO: Step 4
+
+    // If you try to get the next value while at the end of the iterator, the return value is `None`
+    assert_eq!(my_iterable_fav_fruits.next(), None);
 }
