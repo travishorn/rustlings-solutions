fd919d70d561967c009d84d0a6183e59124861f7
Travis Horn <travis@travishorn.com>
2023-03-10 13:34:40 -0600

    Complete lifetimes2

--- lifetimes/lifetimes2.rs
+++ lifetimes/lifetimes2.rs
@@ -1,12 +1,5 @@
-// lifetimes2.rs
-//
-// So if the compiler is just validating the references passed
-// to the annotated parameters and the return type, what do
-// we need to change?
-//
-// Execute `rustlings hint lifetimes2` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
+// The compiler is just validating the references passed to the annotated parameters and the return
+// type.
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
@@ -18,9 +11,14 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
 
 fn main() {
     let string1 = String::from("long string is long");
+    // `string2` must be up here (outside the inner block) so that it can be used when printing the
+    // `result` below.
+    let string2 = String::from("xyz");
     let result;
     {
-        let string2 = String::from("xyz");
+        // When `string2` was in this inner block, we got a compile error because it only lives
+        // until the end of this inner block. When it died, you could not print `result` because
+        // `result` was the result of `longest()` here, which relied on `string2`.
         result = longest(string1.as_str(), string2.as_str());
     }
     println!("The longest string is '{}'", result);
