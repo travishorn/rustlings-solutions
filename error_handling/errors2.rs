752e07b17ed7e0a73e2e7b4d779ab2315ec028a3
Travis Horn <travis@travishorn.com>
2023-03-10 08:31:49 -0600

    Complete errors2

--- error_handling/errors2.rs
+++ error_handling/errors2.rs
@@ -1,23 +1,12 @@
-// errors2.rs
-// Say we're writing a game where you can buy items with tokens. All items cost
-// 5 tokens, and whenever you purchase items there is a processing fee of 1
-// token. A player of the game will type in how many items they want to buy,
-// and the `total_cost` function will calculate the total number of tokens.
-// Since the player typed in the quantity, though, we get it as a string-- and
-// they might have typed anything, not just numbers!
+// Say we're writing a game where you can buy items with tokens. All items cost 5 tokens, and
+// whenever you purchase items there is a processing fee of 1 token. A player of the game will type
+// in how many items they want to buy, and the `total_cost` function will calculate the total number
+// of tokens. Since the player typed in the quantity, though, we get it as a string-- and they might
+// have typed anything, not just numbers!
 
-// Right now, this function isn't handling the error case at all (and isn't
-// handling the success case properly either). What we want to do is:
-// if we call the `parse` function on a string that is not a number, that
-// function will return a `ParseIntError`, and in that case, we want to
-// immediately return that error from our function and not try to multiply
-// and add.
-
-// There are at least two ways to implement this that are both correct-- but
-// one is a lot shorter!
-// Execute `rustlings hint errors2` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
+// This function handles the error case. if we call the `parse` function on a string that is not a
+// number, that function will return a `ParseIntError`, and in that case, we immediately return that
+// error from our function and not try to multiply and add.
 
 use std::num::ParseIntError;
 
@@ -26,7 +15,19 @@ pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let cost_per_item = 5;
     let qty = item_quantity.parse::<i32>();
 
-    Ok(qty * cost_per_item + processing_fee)
+    // Use a match expression to determine if the result of the `parse` was an error. If so, return
+    // that same error (aka propogating the error). Otherwise, if the result is `Ok`, do the math.
+    match qty {
+        Err(e) => Err(e),
+        Ok(num) => Ok(num * cost_per_item + processing_fee),
+    }
+
+    // There is a shortcut to doing the above, since all we're doing is propogating the error if one
+    // exists. You can add a `?` operator to the `parse`.
+    // let qty = item_quantity.parse::<i32>()?;
+    //
+    // And then you can do away with the match expression. Just do the math. `qty` now contains the
+    // i32 instead of `Result<i32, ParseIntError>`. Ok(qty * cost_per_item + processing_fee)
 }
 
 #[cfg(test)]
