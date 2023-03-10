504876c525f0aec74175051f0f93ffd0c0490624
Travis Horn <travis@travishorn.com>
2023-03-10 09:41:00 -0600

    Complete errors6

--- error_handling/errors6.rs
+++ error_handling/errors6.rs
@@ -1,14 +1,7 @@
-// errors6.rs
-
-// Using catch-all error types like `Box<dyn error::Error>` isn't recommended
-// for library code, where callers might want to make decisions based on the
-// error content, instead of printing it out or propagating it further. Here,
-// we define a custom error type to make it possible for callers to decide
-// what to do next when our function returns an error.
-
-// Execute `rustlings hint errors6` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
+// Using catch-all error types like `Box<dyn error::Error>` isn't recommended for library code,
+// where callers might want to make decisions based on the error content, instead of printing it out
+// or propagating it further. Here, we define a custom error type to make it possible for callers to
+// decide what to do next when our function returns an error.
 
 use std::num::ParseIntError;
 
@@ -23,18 +16,20 @@ impl ParsePosNonzeroError {
     fn from_creation(err: CreationError) -> ParsePosNonzeroError {
         ParsePosNonzeroError::Creation(err)
     }
-    // TODO: add another error conversion function here.
-    // fn from_parseint...
+
+    // Provide a function that creates an instance of `ParsePosNonzeroError` from an error of type
+    // `ParseIntError`. It returns the appropriate type.
+    fn from_parseint(err: ParseIntError) -> ParsePosNonzeroError {
+        ParsePosNonzeroError::ParseInt(err)
+    }
 }
 
 fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
-    // TODO: change this to return an appropriate error instead of panicking
-    // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
+    // Try to parse the string slice into an i64. Use `map_err` to map the error returned by
+    // `parse()` to `ParsePosNonzeroError::ParseInt`. Use `?` to propagate the error to the caller.
+    let x: i64 = s.parse().map_err(ParsePosNonzeroError::from_parseint)?;
     PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
 }
-
-// Don't change anything below this line.
 
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
