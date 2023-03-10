61e1e35194be07545d75d2a01067fe0b84df5ea3
Travis Horn <travis@travishorn.com>
2023-03-10 11:28:31 -0600

    Complete traits5

--- traits/traits5.rs
+++ traits/traits5.rs
@@ -1,11 +1,3 @@
-// traits5.rs
-//
-// Your task is to replace the '??' sections so the code compiles.
-// Don't change any line other than the marked one.
-// Execute `rustlings hint traits5` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
-
 pub trait SomeTrait {
     fn some_function(&self) -> bool {
         true
@@ -26,8 +18,10 @@ impl OtherTrait for SomeStruct {}
 impl SomeTrait for OtherStruct {}
 impl OtherTrait for OtherStruct {}
 
-// YOU MAY ONLY CHANGE THE NEXT LINE
-fn some_func(item: ??) -> bool {
+// This function uses methods `some_function()` and `other_function` on the given item. These
+// functions are implemented on two traits: `SomeTrait` and `OtherTrait`. To make sure both traits
+// are implemented on the given item, we can annotate them with `impl` and `+`
+fn some_func(item: impl SomeTrait + OtherTrait) -> bool {
     item.some_function() && item.other_function()
 }
 
