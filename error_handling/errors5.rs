4a7044c057d4c692c726030ffc3f8a04f1e1b721
Travis Horn <travis@travishorn.com>
2023-03-10 09:13:22 -0600

    Complete errors5

--- error_handling/errors5.rs
+++ error_handling/errors5.rs
@@ -2,35 +2,32 @@
 
 // This program uses an altered version of the code from errors4.
 
-// This exercise uses some concepts that we won't get to until later in the course, like `Box` and the
-// `From` trait. It's not important to understand them in detail right now, but you can read ahead if you like.
-// For now, think of the `Box<dyn ...>` type as an "I want anything that does ???" type, which, given
-// Rust's usual standards for runtime safety, should strike you as somewhat lenient!
+// Think of the `Box<dyn ...>` type as an "I want anything that does ???" type, which, given Rust's
+// usual standards for runtime safety, is somewhat lenient!
 
-// In short, this particular use case for boxes is for when you want to own a value and you care only that it is a
-// type which implements a particular trait. To do so, The Box is declared as of type Box<dyn Trait> where Trait is the trait
-// the compiler looks for on any value used in that context. For this exercise, that context is the potential errors
-// which can be returned in a Result.
+// In short, this particular use case for boxes is for when you want to own a value and you care
+// only that it is a type which implements a particular trait. To do so, The Box is declared as of
+// type Box<dyn Trait> where Trait is the trait the compiler looks for on any value used in that
+// context. For this exercise, that context is the potential errors which can be returned in a
+// Result.
 
-// What can we use to describe both errors? In other words, is there a trait which both errors implement?
-
-// Execute `rustlings hint errors5` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
+// `error:Error` can be used to describe both errors. In other words, `error::Error` is a trait
+// which both errors implement.
 
 use std::error;
 use std::fmt;
 use std::num::ParseIntError;
 
-// TODO: update the return type of `main()` to make this compile.
-fn main() -> Result<(), Box<dyn ???>> {
+// The failure type of the return value on this function is a `Box<dyn ???>`. "???" must be a trait
+// that both errors that could be returned implement. `parse` and `PositiveNonzeroInteger::new()`
+// both implement the `error::Error` trait, so we can use that trait in our `Box<dyn ???>` failure
+// type.
+fn main() -> Result<(), Box<dyn error::Error>> {
     let pretend_user_input = "42";
     let x: i64 = pretend_user_input.parse()?;
     println!("output={:?}", PositiveNonzeroInteger::new(x)?);
     Ok(())
 }
-
-// Don't change anything below this line.
 
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
