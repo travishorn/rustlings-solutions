64fa35289114c08162d9f0b5a10cec95b9c5cc15
Travis Horn <travis@travishorn.com>
2023-03-13 17:56:57 -0500

    Complete iterators3

--- iterators/iterators3.rs
+++ iterators/iterators3.rs
@@ -1,13 +1,3 @@
-// iterators3.rs
-// This is a bigger exercise than most of the others! You can do it!
-// Here is your mission, should you choose to accept it:
-// 1. Complete the divide function to get the first four tests to pass.
-// 2. Get the remaining tests to pass by completing the result_with_list and
-//    list_of_results functions.
-// Execute `rustlings hint iterators3` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
-
 #[derive(Debug, PartialEq, Eq)]
 pub enum DivisionError {
     NotDivisible(NotDivisibleError),
@@ -23,21 +13,40 @@ pub struct NotDivisibleError {
 // Calculate `a` divided by `b` if `a` is evenly divisible by `b`.
 // Otherwise, return a suitable error.
 pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
-    todo!();
+    // If the divisor is 0...
+    if b == 0 {
+        // Early return Err() with the correct error type
+        return Err(DivisionError::DivideByZero);
+    }
+
+    // If the remainder of the quotient is not 0...
+    if a % b != 0 {
+        // The dividend is not evenly divisible by the divisor. Early return Err() with the correct
+        // error type
+        return Err(DivisionError::NotDivisible(NotDivisibleError {
+            dividend: a,
+            divisor: b,
+        }));
+    }
+
+    // If we get here, the result is not a DivisionError. Return Ok() with the quotient
+    Ok(a / b)
 }
 
-// Complete the function and return a value of the correct type so the test passes.
+// Return a vector wrapped in a `Result`
 // Desired output: Ok([1, 11, 1426, 3])
-fn result_with_list() -> () {
+fn result_with_list() -> Result<Vec<i32>, DivisionError> {
     let numbers = vec![27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    numbers.into_iter().map(|n| divide(n, 27)).collect()
 }
 
-// Complete the function and return a value of the correct type so the test passes.
+// Return a vector of values each wrapped in `Result`. Although the code inside the function is
+// identical to the one before it, the behavior of `collect()` changes based on the return type of
+// the function.
 // Desired output: [Ok(1), Ok(11), Ok(1426), Ok(3)]
-fn list_of_results() -> () {
+fn list_of_results() -> Vec<Result<i32, DivisionError>> {
     let numbers = vec![27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    numbers.into_iter().map(|n| divide(n, 27)).collect()
 }
 
 #[cfg(test)]
