2132ead206f44d8c08cb5645260b78a570ddb6a4
Travis Horn <travis@travishorn.com>
2023-03-14 13:27:36 -0500

    Complete from_str

--- conversions/from_str.rs
+++ conversions/from_str.rs
@@ -1,10 +1,8 @@
-// from_str.rs
-// This is similar to from_into.rs, but this time we'll implement `FromStr`
-// and return errors instead of falling back to a default value.
-// Additionally, upon implementing FromStr, you can use the `parse` method
-// on strings to generate an object of the implementor type.
+// This is similar to from_into.rs, but this time we implement `FromStr` and return errors instead
+// of falling back to a default value. Additionally, upon implementing FromStr, we use the `parse`
+// method on strings to generate an object of the implementor type.
+
 // You can read more about it at https://doc.rust-lang.org/std/str/trait.FromStr.html
-// Execute `rustlings hint from_str` or use the `hint` watch subcommand for a hint.
 
 use std::num::ParseIntError;
 use std::str::FromStr;
@@ -28,24 +26,47 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
-// Steps:
-// 1. If the length of the provided string is 0, an error should be returned
-// 2. Split the given string on the commas present in it
-// 3. Only 2 elements should be returned from the split, otherwise return an error
-// 4. Extract the first element from the split operation and use it as the name
-// 5. Extract the other element from the split operation and parse it into a `usize` as the age
-//    with something like `"4".parse::<usize>()`
-// 6. If while extracting the name and the age something goes wrong, an error should be returned
-// If everything goes well, then return a Result of a Person object
-//
 // As an aside: `Box<dyn Error>` implements `From<&'_ str>`. This means that if you want to return a
 // string error message, you can do so via just using return `Err("my error message".into())`.
 
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        // If the length of the provided string is 0, an error should be returned
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        }
+
+        // Split the given string on the commas present in it
+        let split: Vec<_> = s.split(',').collect();
+
+        // Only 2 elements should be returned from the split, otherwise return an error
+        if split.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+
+        // Extract the first element from the split operation and use it as the name
+        let name = split[0];
+
+        // If the name is empty, then return an error
+        if name == "" {
+            return Err(ParsePersonError::NoName);
+        }
+
+        // Extract the other element from the split operation
+        let age = split[1]
+            // parse it into a `usize` as the age
+            .parse::<usize>()
+            // If while extracting the age something goes wrong, an error should be returned. Use
+            // the `map_err` method of `Result` to wrap the error. Propagate errors using the  `?`
+            // operator
+            .map_err(ParsePersonError::ParseInt)?;
+
+        // If everything goes well, then return a Result of a Person object
+        Ok(Person {
+            name: name.to_string(),
+            age,
+        })
     }
 }
 
