3d88b9be49d08f5c630f179b2d55301a8caba96f
Travis Horn <travis@travishorn.com>
2023-03-14 09:59:40 -0500

    Complete box1

--- smart_pointers/box1.rs
+++ smart_pointers/box1.rs
@@ -1,26 +1,20 @@
-// box1.rs
-//
-// At compile time, Rust needs to know how much space a type takes up. This becomes problematic
-// for recursive types, where a value can have as part of itself another value of the same type.
-// To get around the issue, we can use a `Box` - a smart pointer used to store data on the heap,
-// which also allows us to wrap a recursive type.
+// At compile time, Rust needs to know how much space a type takes up. This becomes problematic for
+// recursive types, where a value can have as part of itself another value of the same type. To get
+// around the issue, we can use a `Box` - a smart pointer used to store data on the heap, which also
+// allows us to wrap a recursive type.
 //
 // The recursive type we're implementing in this exercise is the `cons list` - a data structure
 // frequently found in functional programming languages. Each item in a cons list contains two
 // elements: the value of the current item and the next item. The last item is a value called `Nil`.
 //
-// Step 1: use a `Box` in the enum definition to make the code compile
-// Step 2: create both empty and non-empty cons lists by replacing `todo!()`
-//
-// Note: the tests should not be changed
-//
-// Execute `rustlings hint box1` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
+// Here we use a `Box` in the enum definition and create both empty and non-empty cons lists
 
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    // Wrap `List` in the `Box` smart pointer type to allow data allocation on the heap. Boxes have
+    // a known size, so Rust doesn't complain about a recursive tpe with infinite size using this
+    // method.
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -33,11 +27,15 @@ fn main() {
 }
 
 pub fn create_empty_list() -> List {
-    todo!()
+    // Use the enum variant `Nil` to return an empty list
+    List::Nil
 }
 
 pub fn create_non_empty_list() -> List {
-    todo!()
+    // Use the enum variant `Cons(...)` to return a non-empty list. The first element must be an i32
+    // and the second must be a `Box<List>`. We can end the recursion right here by making the
+    // second element an empty list
+    List::Cons(0, Box::new(List::Nil))
 }
 
 #[cfg(test)]
