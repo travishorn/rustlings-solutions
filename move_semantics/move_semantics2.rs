cc5a4dd3a7124aeb3c36f0cc79da282d7030d124
Travis Horn <travis@travishorn.com>
2023-03-13 21:35:36 -0500

    move_semantics2 method 2

--- move_semantics/move_semantics2.rs
+++ move_semantics/move_semantics2.rs
@@ -1,13 +1,12 @@
 fn main() {
     let vec0 = Vec::new();
 
-    // Method 1: Make another, separate version of  the data that's in `vec0` and pass that to
-    // `fill_vec` instead.
+    // Method 2: Make `fill_vec` borrow its argument instead of taking ownership of it, and then
+    // copy the data within the function in order to return an owned `Vec<i32>`
     //
     // Once we pass `vec0` to `fill_vec()`, we can no longer access it in this scope. We can fix
-    // this by using the `clone` method, which makes a separate copy of the data that's in `vec0`
-    // and pass that instead
-    let mut vec1 = fill_vec(vec0.clone());
+    // this by passing it as a reference
+    let mut vec1 = fill_vec(&vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -17,8 +16,10 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+// Change the argument type to accept a reference
+fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
+    // Shadow the `vec` variable with a clone of it's original value
+    let mut vec = vec.clone();
 
     vec.push(22);
     vec.push(44);
